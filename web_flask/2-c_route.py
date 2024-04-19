e699bc414620fd84d37d844bddbc9470e13fc505
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-19 12:31:42 +0100

    script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,6 +2,7 @@
 """A script that starts a Flask web application"""
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -16,6 +17,12 @@ def index():
 def hbnb():
     """Route that displays 'HBNB'"""
     return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Route that displays 'C ' followed by the value of the text variable"""
+    return f"C {escape(text.replace('_', ' '))}"
 
 
 if __name__ == '__main__':
