5f2b07006b6fc87c9d88aba7d5616313b33bbf57
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-19 20:30:30 +0100

    Improve engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,4 +76,4 @@ class DBStorage:
     def close(self):
         """Call remove() method on the private session attribute
         (self.__session)"""
-        self.__session.remove()
+        self.__session.close()
