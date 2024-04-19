c5e2474098d51f36fb380c1689015ceaa73ad27b
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-19 12:37:35 +0100

    script that starts a Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route that displays 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route that displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Route that displays 'C ' followed by the value of the text variable"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
