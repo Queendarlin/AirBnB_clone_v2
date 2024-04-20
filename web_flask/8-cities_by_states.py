d2f6b17c5e57cf78a3396f7fa3bfd08a24083349
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-20 20:21:09 +0100

    Adjustments

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" Cities by States Module for HBNB project """
+"""Start web application with two routes
+"""
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Display a list of states and cities """
+@app.route('/cities_by_states')
+def states_list():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
     states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Remove the current SQLAlchemy Session"""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
