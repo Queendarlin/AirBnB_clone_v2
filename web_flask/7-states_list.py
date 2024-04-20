1760f36ddf13760be5f6748e30718afcb5f22549
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-20 06:48:53 +0100

    script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,12 +8,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a list of states in HTML format"""
@@ -21,6 +15,12 @@ def states_list():
     sorted_states = sorted(states.values(), key=lambda x: x.name)
 
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
