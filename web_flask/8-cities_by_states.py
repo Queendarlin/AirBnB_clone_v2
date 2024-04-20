a3ccf2c2e7871bfbfab5de75fc8608655078eda7
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-20 19:23:23 +0100

    Adjustments

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Cities by States Module for HBNB project """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Display a list of states and cities """
+    sorted_states = sorted(storage.all("State").values(), key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
