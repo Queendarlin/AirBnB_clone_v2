26608134bfa6f28d7c6cb30740ba882e81ab82fa
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-20 20:03:22 +0100

    Adjustments

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,14 +4,18 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Display a list of states and cities """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+@app.route('/states', strict_slashes=False)
+def list_states():
+    """ Display a list of states sorted by name """
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=states_sorted)
+@app.route('/states/<id>', strict_slashes=False)
+def state_id():
 
 
 @app.teardown_appcontext
