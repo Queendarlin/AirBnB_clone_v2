c2d774ecc52fa4dd7656a2e1ab3370dae73abc04
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-20 08:33:31 +0100

    Adjustments

--- models/place.py
+++ models/place.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import relationship
 from os import getenv
 import shlex
 import models
-from models.amenity import Amenity
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
@@ -33,38 +32,40 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
 
         amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False)
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         @property
         def reviews(self):
             """ Returns list of reviews.id """
-            all_reviews = models.storage.all(Review)
-            place_reviews = []
-            for review_ins in all_reviews.values():
-                if review_ins.place_id == self.id:
-                    place_reviews.append(review_ins)
-
-            return place_reviews
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if review[0] == 'Review':
+                    lista.append(var[key])
+            for elem in lista:
+                if elem.place_id == self.id:
+                    result.append(elem)
+            return result
 
         @property
         def amenities(self):
             """ Returns list of amenity ids """
-            all_amenities = models.storage.all(Amenity)
-            place_amenities = []
-            for amenity_ins in all_amenities.values():
-                if amenity_ins.place_id == self.id:
-                    place_amenities.append(amenity_ins)
-
-            return place_amenities
+            return self.amenity_ids
 
         @amenities.setter
-        def amenities(self, amenity_obj):
+        def amenities(self, obj=None):
             """ Appends amenity ids to the attribute """
-            if isinstance(amenity_obj, models.Amenity):
-                self.amenities.append(amenity_obj.id)
+            from models.amenity import Amenity
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
